1240ea20e1c96b40dbc0c582ecb32dcaa3f4f911
omerfarukbala <omerfarukbala@gmail.com>
2024-04-16 14:45:14 +0300

    project created

---
+++ src/main/java/com/tobeto/java4a/pair4lms/Pair4lmsApplication.java
@@ -0,0 +1,13 @@
+package com.tobeto.java4a.pair4lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pair4lmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pair4lmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tobeto/java4a/pair4lms/Pair4lmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobeto.java4a.pair4lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pair4lmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
